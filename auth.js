4976bc27d1ccc69a280b5cc44eca16c729fb59c3
gzmox <mush.tbk@gmail.com>
2025-07-05 19:12:35 +0200

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,21 @@
+// auth.js (brukes både på login.html og index.html)
+
+const validUsers = [
+  { username: 'admin', password: 'hemmelig' },
+  { username: 'montør', password: 'kontroll' }
+];
+
+function login(username, password) {
+  const found = validUsers.find(u => u.username === username && u.password === password);
+  if (found) {
+    localStorage.setItem('isLoggedIn', 'true');
+    return true;
+  }
+  return false;
+}
+
+function checkAuth() {
+  if (localStorage.getItem('isLoggedIn') !== 'true') {
+    window.location.href = 'login.html';
+  }
+}
